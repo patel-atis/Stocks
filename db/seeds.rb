4e61d765b07f269e12fd379355fbdfd5a730da23
Atish <atish.patel95@yahoo.com>
2018-03-21 16:08:32 -0400

    seed stocks database with 100 stocks from wsj

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+
+#this will store all of the companies scraped with their ticker symbol
+companies = []
+
+#get companies list
+doc = Nokogiri::HTML(open('http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html'))
+
+#iterate through the companies and split stock name and ticker
+#append both values (as an array) into the companies array
+doc.css('td a.linkb').each do |f|
+  full_name = f.text
+  name, ticker = full_name.split(' (')
+  ret_arr = [name, ticker.chomp().chop()]
+  companies << ret_arr
+end
+
+#for each company create a value in the stocks database
+companies.each do |comp|
+  Stock.create(company: comp[0], ticker: comp[1], price: 0, dividend: 0, type: false, rating: "A")
+end
