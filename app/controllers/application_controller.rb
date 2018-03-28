3c375fcddc07aa57fa05bf87e9519714cc7ee350
Atish <atish.patel95@yahoo.com>
2018-03-27 22:03:55 -0400

    create global controller current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  #declare a current user method for all controllers
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
 end
