b76df8dc8fefda8138e82535ae11897846c0d066
Atish <atish.patel95@yahoo.com>
2018-03-27 22:04:12 -0400

    add name and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :stocks, through: :ledger
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 249}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
