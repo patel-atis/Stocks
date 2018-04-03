d908306d67fc48ebaaf2bfe0c4c008195a7d84eb
Atish <atish.patel95@yahoo.com>
2018-04-03 15:53:41 -0400

    add bootstrap style and apply to nav

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( nav.css.scss )
+Rails.application.config.assets.precompile += %w( sidebar.css bootstrap.css nav.css)
