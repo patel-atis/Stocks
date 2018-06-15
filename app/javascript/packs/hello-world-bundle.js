d0dd3f55ff36ce642a64859787da2d04bc67b381
Atish <atish.patel95@yahoo.com>
2018-06-15 13:54:11 -0400

    delete the default react on rails hello world component

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,12 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 import StockRow from '../bundles/Stocks/components/StockRow';
 import StockList from '../bundles/Stocks/components/StockList';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
   StockRow,
   StockList
 });
